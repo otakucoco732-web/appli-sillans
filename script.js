b0563ef42f93042842495439052c9568f514760f
otakucoco732-web <otakucoco732@gmail.com>
2026-04-01 16:30:16 +0200

    First commit

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("coucou")
+//Variables
+let maVar = "La valeur" //valeur qui change
+const maConst = "Autre valeur" //ne changera jamais
+
+// Structure conditionnelles
+//if (condition et condition ou (condition et condition))
+if(maVar == "titi"){
+    console.log("C'est ok")
+} else if(maVar == "La valeur"){
+    console.log("Autre cas")
+} else {
+    console.log("Tant pis")
+}
+
+switch(maVar){
+    case "titi":
+        console.log("C'est ok")
+    break;
+    case "La valeur":
+        console.log("Autre cas")
+    break;
+    default:
+        console.log("Tant pis")
+}
+
+// Boucles
+for(let i = 0; i<100; i+2){
+    console.log("Nombres pairs :" +i)
+}
+
+// Fonctions
+
+// Classes
